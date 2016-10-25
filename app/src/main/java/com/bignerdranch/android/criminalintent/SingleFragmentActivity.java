d92e13944eddb874db2a61e952c1accbf9ee6ec4
Marley Alford <ma8642@bard.edu>
2016-10-25 10:31:44 -0400

    Implemented up to p. 237.  We have changed all fragments to be subclasses of AppCompatActivity.

--- app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -4,11 +4,13 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by MarleyAlford on 10/13/16.
  */
-public abstract class SingleFragmentActivity extends FragmentActivity {
+//public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {  //change class to be a subclass of appcompatactivity
     protected abstract Fragment createFragment();
 
     @Override
