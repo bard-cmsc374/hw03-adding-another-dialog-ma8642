891faf89ffb548e3d638895c73201c8e57af283a
Marley Alford <ma8642@bard.edu>
2016-11-04 23:14:42 -0400

    Implemented up to p. 311.  Two pane interface is working on tablet!

--- app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/SingleFragmentActivity.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -13,10 +14,15 @@ import android.support.v7.app.AppCompatActivity;
 public abstract class SingleFragmentActivity extends AppCompatActivity {  //change class to be a subclass of appcompatactivity
     protected abstract Fragment createFragment();
 
+    @LayoutRes
+    protected int getLayoutResId() {  //get id for whether to be in phone or tablet mode
+        return R.layout.activity_fragment;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
+        //setContentView(R.layout.activity_fragment); //this was when we assumed we were always on a phone
+        setContentView(getLayoutResId());
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragment_container);
