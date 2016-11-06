5ab3b248ec63a524f9d13f122788a9228a65cbd9
Marley Alford <ma8642@bard.edu>
2016-11-06 12:46:07 -0500

    Fully Implemented Chapter 17.

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -3,6 +3,7 @@ package com.bignerdranch.android.criminalintent;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -71,6 +72,22 @@ public class CrimeListFragment extends Fragment {
 
         return view;
     }
+    
+    //Response to Query  https://developer.android.com/training/permissions/requesting.html
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[]
+//            permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions,
+//                grantResults);
+//        List<Fragment> fragments = getSupportFragmentManager().getFragments();
+//        if (fragments != null) {
+//            for (Fragment fragment : fragments) {
+//                fragment.onRequestPermissionsResult(requestCode, permissions,
+//                        grantResults);
+//            }
+//        }
+//    }
 
     @Override
     public void onResume() {
